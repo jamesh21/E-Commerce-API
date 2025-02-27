94e3827f652c4e094d492bc4325fdf8b2f66de43
James Ho <jamessho821@gmail.com>
2025-02-27 00:12:40 -0800

    added product list and product card components

---
+++ frontend/src/App.jsx
@@ -0,0 +1,16 @@
+import { ProductList } from './components/ProductList';
+import { NavBar } from './components/NavBar'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+
+function App() {
+    return (
+        <>
+            <NavBar></NavBar>
+            <ProductList></ProductList>
+        </>
+
+    );
+}
+
+export default App;
