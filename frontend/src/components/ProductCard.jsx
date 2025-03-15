65807e3824da0c20ef69577f7ec560aaa8a43ca5
James Ho <jamessho821@gmail.com>
2025-03-15 15:10:26 -0700

    fixed uneven height of where button is placed in products

--- frontend/src/components/ProductCard.jsx
+++ frontend/src/components/ProductCard.jsx
@@ -17,32 +17,32 @@ function ProductCard({ product, handleAddToCart }) {
 
     return (
         <Card className="shadow product-card h-100">
-            <Card.Body className="product-card-body">
-                <Card.Img variant="top" alt={product.productName} src={product.imageUrl || "https://plus.unsplash.com/premium_photo-1734543932103-37f616c1b0b1?q=80&w=2560&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"}></Card.Img>
-                <div className="product-card-text">
-                    <Card.Title>{product.productName}</Card.Title>
-                    <Card.Subtitle>
-                        {product.stock === 0 ? "Out of stock" : <>${product.price}</>}
-                    </Card.Subtitle>
-                    <Card.Text>
-                        {product.stock <= 5 && product.stock > 0 && <>Low stock {product.stock} left</>}
-                    </Card.Text>
-                    <Button variant={product.stock === 0 ? "secondary" : "primary"} onClick={handleClick} disabled={loading || product.stock === 0}>
-                        {loading ? (<>
-                            <Spinner
-                                as="span"
-                                animation="border"
-                                size="sm"
-                                role="status"
-                                aria-hidden="true"
-                                className="me-2"
-                            />
-                            Adding...
-                        </>)
-                            : ("Add to Cart")
-                        }
-                    </Button>
-                </div>
+            <Card.Img variant="top" alt={product.productName} src={product.imageUrl || "https://plus.unsplash.com/premium_photo-1734543932103-37f616c1b0b1?q=80&w=2560&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"}></Card.Img>
+            <Card.Body className="d-flex flex-column p-3">
+
+                <Card.Title>{product.productName}</Card.Title>
+                <Card.Subtitle>
+                    {product.stock === 0 ? "Out of stock" : <>${product.price}</>}
+                </Card.Subtitle>
+                <Card.Text>
+                    {product.stock <= 5 && product.stock > 0 && <>Low stock {product.stock} left</>}
+                </Card.Text>
+
+                <Button className="mt-auto mb-1 mx-2" variant={product.stock === 0 ? "secondary" : "dark"} onClick={handleClick} disabled={loading || product.stock === 0}>
+                    {loading ? (<>
+                        <Spinner
+                            as="span"
+                            animation="border"
+                            size="sm"
+                            role="status"
+                            aria-hidden="true"
+                            className="me-2"
+                        />
+                        Adding...
+                    </>)
+                        : ("Add to Cart")
+                    }
+                </Button>
             </Card.Body>
         </Card>
     )
